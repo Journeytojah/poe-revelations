35a596bf139efaeb57a7e84c42b265e26c35830b
OneRobotBoii <onerootboii@proton.me>
2025-01-30 21:16:20 +0700

    back to local schema

--- src/lib/utils/fetchSchema.ts
+++ src/lib/utils/fetchSchema.ts
@@ -2,7 +2,8 @@ import type { SchemaFile, SchemaTable } from 'pathofexile-dat-schema';
 
 export async function fetchSchema(fetch: (input: RequestInfo | URL) => Promise<Response>): Promise<SchemaFile | null> {
   try {
-    const url = 'https://github.com/poe-tool-dev/dat-schema/releases/download/latest/schema.min.json';
+    // const url = 'https://github.com/poe-tool-dev/dat-schema/releases/download/latest/schema.min.json';
+    const url = './schema.min.json';
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Failed to fetch schema: ${response.status}`);
